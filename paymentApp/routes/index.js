b5b2ff2a0d11a6e09d8a80c10f8e23eca0c5ee87
Kunal Kumar Arya <kunalkrraj220@gmail.com>
2023-04-03 18:44:24 +0530

    payment test mode

--- paymentApp/routes/index.js
+++ paymentApp/routes/index.js
@@ -6,8 +6,8 @@ const Razorpay = require('razorpay');
 
 //Instantiate the Razorpay Instance
 var instance = new Razorpay({
-  key_id: 'rzp_test_r6EJwrxJbscSV9',
-  key_secret: 'A0ETOcXCfUXgupemi1uCptXr',
+  key_id: 'rzp_test_9PGHW4frZyI7ng',
+  key_secret: 'dWggFPupztlUtQQIoQkmFHlf',
 });
 
 
@@ -16,6 +16,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/create/orderId',(req,res) =>{
+
   var options = {
     amount: 50000,  // amount in the smallest currency unit
     currency: "INR",
@@ -25,6 +26,23 @@ router.post('/create/orderId',(req,res) =>{
     console.log(order);
     return res.send(order);
   });
-})
+});
+
+router.post("/api/payment/verify",(req,res)=>{
+
+  let body=req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+ 
+   var crypto = require("crypto");
+   var expectedSignature = crypto.createHmac('sha256', 'dWggFPupztlUtQQIoQkmFHlf')
+      .update(body.toString())
+      .digest('hex');
+      console.log("sig received " ,req.body.response.razorpay_signature);
+      console.log("sig generated " ,expectedSignature);
+   var response = {"signatureIsValid":"false"}
+   if(expectedSignature === req.body.response.razorpay_signature)
+    response={"signatureIsValid":"true"}
+       res.send(response);
+   });
+
 
 module.exports = router;
